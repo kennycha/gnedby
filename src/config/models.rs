8078304da3b6b8721385aa29efe2d75a8c4b7bc1
kennycha <dudqn136@naver.com>
2025-05-07 19:00:07 +0900

    fix: remove redundant default

--- src/config/models.rs
+++ src/config/models.rs
@@ -1,20 +1,9 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
 pub struct SyncConfig {
     pub storage_url: Option<String>,
     pub token: Option<String>,
     pub last_sync: Option<String>,
     pub auto_sync: bool,
-}
-
-impl Default for SyncConfig {
-    fn default() -> Self {
-        Self {
-            storage_url: None,
-            token: None,
-            last_sync: None,
-            auto_sync: false,
-        }
-    }
 }
